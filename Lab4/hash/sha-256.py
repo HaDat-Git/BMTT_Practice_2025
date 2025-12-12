48c03529120c29af46467d1701c00f6781baa2b2
HaDat-Git <hadat910@gmail.com>
2025-12-12 14:35:28 +0700

    add lab04 hash

---
+++ Lab4/hash/sha-256.py
@@ -0,0 +1,10 @@
+import hashlib
+
+def calculate_sha256_hash(input_string):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(input_string.encode('utf-8'))
+    return sha256_hash.hexdigest()
+
+data_to_hash = input("Enter the string to hash: ")
+hash_value = calculate_sha256_hash(data_to_hash)
+print(f"SHA-256 hash of the string '{data_to_hash}' is: {hash_value}")
